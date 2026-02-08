ccfd8ed648a648967208758a900f7e46be0589f0
Snake <238378466+snake0227@users.noreply.github.com>
2026-02-09 00:18:53 +0900

    Initial commit

---
+++ bio_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: Snake
+# SPDX-Lisence-Identifier: BSD-3-Clause
+
+import random
+
+quiz_data = [
+                {"q": "", "a": ""},
+                {"q": "", "a": ""},
+                {"q": "", "a": ""},
+                {"q": "", "a": ""},
+            ]
